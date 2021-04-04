7ffee91af589c1fd53fa22dd12f6e81b3ea33d45
Tiago Mendes <tetigo@gmail.com>
2021-04-04 15:38:48 -0300

    01-useState-Change Title

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
-};
+  // console.log(useState)
+  // console.log(useState('hi world'))
+  // const value = useState(1)[0]
+  // const handler = useState(1)[1]
+  // console.log(value, handler)
+  const [text, setText] = useState('random title')
 
-export default UseStateBasics;
+  const handleClick = () => {
+    if (text === 'random title') setText('hi world')
+    else setText('random title')
+  }
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button className="btn" onClick={handleClick}>
+        change title
+      </button>
+    </React.Fragment>
+  )
+}
+
+export default UseStateBasics
