9d4670dcc73fb7b16fb25f29745d58e0e178e553
Tiago Mendes <tetigo@gmail.com>
2021-04-06 17:54:04 -0300

    cleanup event listener inside useEffect

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
-};
+  const [size, setSize] = useState(window.innerWidth)
 
-export default UseEffectCleanup;
+  const checkSize = () => {
+    setSize(window.innerWidth)
+  }
+
+  useEffect(() => {
+    console.log('useEffect')
+    //se adicionou event listener mas o hook não tem lista de dependencia
+    //vai ser chamado toda vez, e vai adicionar outros event listeners
+    window.addEventListener('resize', checkSize)
+    //fazendo esse cleanup, retorna um removeEventListener, o que faz componente
+    //que antes do useEffect ser chamado novamente, ele remova o anterior
+    return () => {
+      console.log('cleanup')
+      window.removeEventListener('resize', checkSize)
+    }
+  })
+
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size} PX</h2>
+    </>
+  )
+}
+
+export default UseEffectCleanup
