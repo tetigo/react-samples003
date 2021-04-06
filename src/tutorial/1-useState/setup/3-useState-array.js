4abfd473022120341397b2fd7f42232766f65470
Tiago Mendes <tetigo@gmail.com>
2021-04-05 23:15:24 -0300

    useState with array and object

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from 'react'
+import { data } from '../../../data'
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
-};
+  const [people, setPeople] = React.useState([])
 
-export default UseStateArray;
+  const removeItem = (id) => {
+    const data = people.filter((p) => p.id !== id)
+    setPeople(data)
+  }
+
+  return (
+    <React.Fragment>
+      <button className="btn" onClick={() => setPeople([])}>
+        clear data
+      </button>
+      <h2 onClick={() => setPeople(data)}>Mouse Click Here</h2>
+      <ul>
+        {people.map((people) => (
+          <li className="item">
+            <Person key={people.id} {...people} />
+            <button onClick={() => removeItem(people.id)}>Remove Item</button>
+          </li>
+        ))}
+      </ul>
+    </React.Fragment>
+  )
+}
+
+const Person = ({ name }) => {
+  return <h3>{name}</h3>
+}
+
+export default UseStateArray
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const user = {
+    name: 'tiago',
+    age: 43,
+    message: 'random message',
+  }
+  const [person, setPerson] = useState(user)
+  const handleClick = () => {
+    if (person.name === 'tiago')
+      setPerson({ name: 'marta', age: 31, message: 'loves tiago' })
+    else setPerson({ ...user, message: 'loves marta' })
+  }
+  return (
+    <>
+      <h2>useState object example</h2>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button onClick={handleClick}>change person</button>
+    </>
+  )
+}
 
-export default UseStateObject;
+export default UseStateObject
