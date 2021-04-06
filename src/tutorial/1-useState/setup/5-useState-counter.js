014bed1505a25f1e9811954d89e891b9ae3092a7
Tiago Mendes <tetigo@gmail.com>
2021-04-05 23:51:52 -0300

    update by function - the best way

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -2,6 +2,22 @@ import React, { useState } from 'react'
 
 const UseStateCounter = () => {
   const [value, setValue] = useState(0)
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      //ao clicar varias vezes, em todas as vezes o value atual("original") foi utilizado,
+      //não foi somado + 1 a cada vez que clicou, por causa do assincronismo
+      // setValue(value + 1)
+      // console.log(value)
+      //////////////////////////////////////////
+      //ao clicar varias vezes, o valor atual("atualizado") foi passado como parametro,
+      //nesse caso, em todas as vezes foi somado + 1 a cada clique
+      setValue((prevState) => {
+        console.log(prevState)
+        return prevState + 1
+      }) //prevState pode ser qualquer nome
+    }, 2000)
+  }
   return (
     <>
       <section>
@@ -15,6 +31,13 @@ const UseStateCounter = () => {
         </button>
         <button className="btn" onClick={() => setValue(value + 1)}>
           increase
+        </button>
+      </section>
+      <section>
+        <h2>complex counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease}>
+          increase later
         </button>
       </section>
     </>
